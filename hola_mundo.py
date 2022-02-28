9c21351dea73c69d76bbeadd11426e1a8a92a239
PsychosphereCode <fabian.rottens@gmail.com>
2022-02-28 14:16:09 -0300

    primer commit

---
+++ hola_mundo.py
@@ -0,0 +1,19 @@
+x = 'Hola, mundo'
+print(x)
+
+nombre= "Fabian Bustos"
+
+print("Hola", nombre)
+print ("Hola "+ nombre)
+
+numeroFavorito= 16
+print ("!Hola", numeroFavorito)
+
+print("!Hola " + str(numeroFavorito))
+
+comida_1 = 'sushi'
+comida_2 = 'Papas supremas'
+
+print("Amo comer {} y {}".format(comida_1, comida_2))
+
+print (f"Amo comer {comida_1} y {comida_2}")
